6a4b72572db1bc82989702eb7888fbc020262896
tristen <tristen.urban@gmail.com>
2022-03-10 10:38:57 -0800

    .env reconfig

--- server/pool.js
+++ server/pool.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'tristenu',
-  password: 'sdcstee1ix',
-  database: 'products'
+  host: process.env.HOST || 'localhost',
+  port: process.env.PORT || 5432,
+  user: process.env.USER || 'postgres',
+  password: process.env.PASSWORD || '',
+  database: process.env.DATABASE || 'products'
 });
 
 
