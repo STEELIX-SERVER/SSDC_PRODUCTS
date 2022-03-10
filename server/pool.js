48023c0f4a7cbb352b31a3294e734cfc26b02c2e
tristen <tristen.urban@gmail.com>
2022-03-10 10:56:11 -0800

    Postgres did not like that pool connection

--- server/pool.js
+++ server/pool.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 const pool = new Pool({
   host: process.env.HOST || 'localhost',
   port: process.env.PORT || 5432,
-  user: process.env.USER || 'postgres',
-  password: process.env.PASSWORD || '',
+  user: 'postgres' || process.env.USER,
+  password: process.env.PASSWORD || 'postgres',
   database: process.env.DATABASE || 'products'
 });
 
